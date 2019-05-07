94434aac7a4c6763e04d25bd6e3dcf9a9d7957be
UzuHero <Tchelkanova1@gmail.com>
2019-05-08 00:23:23 +0700

    Izmenen learning.c dobavlena 1 funkz

--- src/learning.c
+++ src/learning.c
@@ -4,7 +4,51 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<string.h>
 #include<strings.h>
+#include<time.h>
 
+
+float nem_lichniy()
+{
+	char *slovanem[9]={"Ich", "Du", "Er", "Sie", "Es", "Wir", "Ihr", "Sie"};
+	char *slovrus[9]={"Я", "Ты", "Он", "Она", "Оно", "Мы","Вы","Они"};
+	int i, n=0, k=0;
+    printf("\v");
+	for(i=0; i<8; i++){
+	printf("      %s\t->\t%s   \n", slovanem[i], slovrus[i]);}
+    printf("\v     Записали и запомнили?\nКогда вы будете готовы, нажмите клавишу \"ENTER\"\n");
+    getchar();
+    system("clear");
+    char *mmm[9];
+    printf("        Подготовтесь к написанию иностранных слов!\n        Пишите с большой буквы!!!\n");
+    clock_t start = clock();
+    for(i=0; i<8; i++)
+    {
+        printf("%s  = ",slovrus[i]);
+        mmm[i]= malloc(MAXLEN);
+        scanf("%s", mmm[i]);
+        printf("Правильный перевод:%s\n", slovanem[i]);
+    }
+    clock_t stop = clock();
+    double elapsed = (double)(stop - start) / CLOCKS_PER_SEC;
+    
+    printf("\nВы ввели данные слова:\n");
+    for(i=0; i<8; i++)
+    {
+        printf("%s\t->\tПравильно:%s\t = \tПеревод:%s\n", mmm[i], slovanem[i], slovrus[i]);
+    }
+    for(i=0;i<8;i++)
+    {
+        if((strcmp(mmm[i], slovanem[i])))
+        n=n+1;
+        if((!strcmp(mmm[i], slovanem[i])))
+        k=k+1;
+    }
+    printf("Верных слов:%d\nНеверные слова:%d\n", k, n);
+    printf("Время написания слов: %f sec\n", elapsed);
+    for(i=0; i<8; i++)
+    free(mmm[i]);
+    return 0;
+}
 ///функция англицкий язык///
 float angl()
 {
@@ -104,34 +148,34 @@ float nem()
     printf("Отлично, Вы выбрали раздел №%d.\nСперва программа выведит иностранные слова с переводом.\nПосле ознакомления с программой, Вы должны будете написать перевод иностранного слова.\n В конце вы узнаете колличество правильно написанных и неправильно написанных слов.\n", razdel);
     switch(razdel){
         case 1:
-        angl_lichniy();
+        nem_lichniy();
         break;
         case 2:
-        angl_glagol();
+        nem_glagol();
         break;
         case 3:
-        angl_semiya();
+        nem_semiya();
         break;
         case 4:
-        angl_fructoovosh();
+        nem_fructoovosh();
         break;
         case 5:
-        angl_edainapitok();
+        nem_edainapitok();
         break;
         case 6:
-        angl_pririda();
+        nem_pririda();
         break;
         case 7:
-        angl_zveta();
+        nem_zveta();
         break;
         case 8:
-        angl_pogoda();
+        nem_pogoda();
         break;
         case 9:
-        angl_zhivotn();
+        nem_zhivotn();
         break;
         case 10:
-        angl_prilagat();
+        nem_prilagat();
         break;
         default:
         printf("Ошибочка\n");
@@ -141,34 +185,34 @@ float nem()
     while(razdel=0){
         switch(razdel){
         case 1:
-        angl_lichniy();
+        nem_lichniy();
         break;
         case 2:
-        angl_glagol();
+        nem_glagol();
         break;
         case 3:
-        angl_semiya();
+        nem_semiya();
         break;
         case 4:
-        angl_fructoovosh();
+        nem_fructoovosh();
         break;
         case 5:
-        angl_edainapitok();
+        nem_edainapitok();
         break;
         case 6:
-        angl_pririda();
+        nem_pririda();
         break;
         case 7:
-        angl_zveta();
+        nem_zveta();
         break;
         case 8:
-        angl_pogoda();
+        nem_pogoda();
         break;
         case 9:
-        angl_zhivotn();
+        nem_zhivotn();
         break;
         case 10:
-        angl_prilagat();
+        nem_prilagat();
         break;
         default:
         printf("Ошибочка\n");
