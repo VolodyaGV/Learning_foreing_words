e7d1a28b2fda627fd820642ce77d9d8af6aaf5a7
VolodyaGV <gvova203@gmail.com>
2019-06-23 16:01:51 +0700

    repair makefile

--- src/learning.h
+++ src/learning.h
@@ -1,4 +1,30 @@
 #ifndef LEARNING_H
 #define LEARNING_H
 
+double wtime();
+float angl_zveta();
+float angl_pogoda();
+float angl_zhivotn();
+float angl_prilag();
+float nem_prilagat();
+float nem_zhivotn();
+float angl_lichniy();
+float nem_pogoda();
+float angl_glagol();
+float nem_priroda();
+float angl_semiya();
+float nem_edainapitok();
+float angl_fructoovosh();
+float nem_semiya();
+float nem_zveta();
+float angl_edainapitok();
+float nem_fructoovosh();
+float angl_priroda();
+float nem_glagol();
+float nem_lichniy();
+void ANGL();
+float angl();
+void NEM();
+float nem();
+
 #endif
