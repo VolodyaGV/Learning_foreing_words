62fc4df43b1f0e169c330ecb3b1d171fbb687704
VolodyaGV <gvova203@gmail.com>
2019-06-25 00:12:14 +0700

    Add new function

--- src/learning.h
+++ src/learning.h
@@ -2,27 +2,30 @@
 #define LEARNING_H
 
 double wtime();
-float angl_zveta(char name[20]);
-float angl_pogoda(char name[20]);
-float angl_zhivotn(char name[20]);
-float angl_prilag(char name[20]);
-float nem_prilagat(char name[20]);
-float nem_zhivotn(char name[20]);
-float angl_lichniy(char name[20]);
-float nem_pogoda(char name[20]);
-float angl_glagol(char name[20]);
-float nem_priroda(char name[20]);
-float angl_semiya(char name[20]);
-float nem_edainapitok(char name[20]);
-float angl_fructoovosh(char name[20]);
-float nem_semiya(char name[20]);
-float nem_zveta(char name[20]);
-float angl_edainapitok(char name[20]);
-float nem_fructoovosh(char name[20]);
-float angl_priroda(char name[20]);
-float nem_glagol(char name[20]);
-float nem_lichniy(char name[20]);
-float angl(char name[20]);
-float nem(char name[20]);
+int check(int i);
+int return_min(double tt);
+int return_sec(double tt, int min);
+int angl_zveta(char name[20]);
+int angl_pogoda(char name[20]);
+int angl_zhivotn(char name[20]);
+int angl_prilag(char name[20]);
+int nem_prilagat(char name[20]);
+int nem_zhivotn(char name[20]);
+int angl_lichniy(char name[20]);
+int nem_pogoda(char name[20]);
+int angl_glagol(char name[20]);
+int nem_priroda(char name[20]);
+int angl_semiya(char name[20]);
+int nem_edainapitok(char name[20]);
+int angl_fructoovosh(char name[20]);
+int nem_semiya(char name[20]);
+int nem_zveta(char name[20]);
+int angl_edainapitok(char name[20]);
+int nem_fructoovosh(char name[20]);
+int angl_priroda(char name[20]);
+int nem_glagol(char name[20]);
+int nem_lichniy(char name[20]);
+int angl(char name[20]);
+int nem(char name[20]);
 
 #endif
